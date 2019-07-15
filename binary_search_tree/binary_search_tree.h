4f0a029408b81da50b51ebcf9a63bff90b0a3e5f
Caio Raposo <caioraposo94@gmail.com>
2019-07-15 19:14:27 -0300

    Move data structures directories

--- binary_search_tree/binary_search_tree.h
+++ binary_search_tree/binary_search_tree.h
@@ -2,20 +2,17 @@
 #define BINARY_SEARCH_TREE_H
 
 
-struct node {
+typedef struct node {
 	int data;
 	struct node *left;
 	struct node *right;
-};
+} node;
 
 
 typedef struct trunk {
 	struct trunk *prev;
 	char str[10];
 } trunk;
-
-
-typedef struct node node;
 
 
 node *insert(node *root, int data);
